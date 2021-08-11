ea6bcfe2bf885150acc3491528ce185f31a078aa
Антон Бурмацкий <antonburmackiy@MacBook-Pro.local>
2021-08-11 18:11:22 +0400

    Version 1.22 Add additional stat on region163_dashboard/ed_centers_empl view

--- region163_dashboard/views.py
+++ region163_dashboard/views.py
@@ -8,9 +8,11 @@ from django.template.defaulttags import register
 def ed_centers_empl(request):
     stat = {}
     stages = ['NEW', 'VER', 'ADM', 'SED', 'COMP', 'NCOM', 'RES']
-    applications = [application for application in Application.objects.filter(appl_status__in = stages).values('competence__title', 'education_center__name', 'appl_status')]
+    applications = [application for application in Application.objects.all().values('competence__title', 'education_center__name', 'appl_status')]
     competencies = [competence for competence in Competence.objects.all().values('title')]
     education_centers = [education_center for education_center in EducationCenter.objects.all().values('name')]
+    education_programs = [education_program for education_program in EducationProgram.objects.all().values('program_name')]
+
 
     for competence in competencies:
         competence = competence['title']
@@ -24,14 +26,19 @@ def ed_centers_empl(request):
                 stat[competence][education_center][stage] = 0
 
     for application in applications:
-        stat[application['competence__title']][application['education_center__name']][application['appl_status']] += 1
-        if stat[application['competence__title']][application['education_center__name']][application['appl_status']] > 0:
-            stat[application['competence__title']]['Empty'] = False
-            stat[application['competence__title']][application['education_center__name']]['Empty'] = False
+        if application['appl_status'] in stages:
+            stat[application['competence__title']][application['education_center__name']][application['appl_status']] += 1
+            if stat[application['competence__title']][application['education_center__name']][application['appl_status']] > 0:
+                stat[application['competence__title']]['Empty'] = False
+                stat[application['competence__title']][application['education_center__name']]['Empty'] = False
             
     return render(request, 'region163_dashboard/ed_centers_empl.html', {
         'stat': stat,
-        'stages': stages
+        'stages': stages,
+        'appl_count': len(applications),
+        'education_centers_count': len(education_centers),
+        'competencies_count': len(competencies),
+        'education_programs_count': len(education_programs)
     })
 
 @register.filter
