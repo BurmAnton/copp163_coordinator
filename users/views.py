4c6733ac4caa0003144a18e2bffcfd5684df022d
Anton <a.burmatsky@gmail.com>
2022-09-06 14:20:07 +0400

    Add import_contacts option

--- users/views.py
+++ users/views.py
@@ -3,40 +3,52 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 
+from federal_empl_program.forms import ImportDataForm
 from .models import PartnerContact, Project, PartnerOrganization, PartnerContactEmail, PartnerContactPhone, PartnerOrganization
-
+from . import imports
 # Create your views here.
 @csrf_exempt
 def contacts_list(request):
+    message = None
+    data = None
     if request.method == 'POST':
-        last_name = request.POST["last_name"]
-        first_name = request.POST["first_name"]
-        middle_name = request.POST["middle_name"]
-        job_title = request.POST["job_title"]
-        organization_id = request.POST["organization"]
-        commentary = request.POST["commentary"]
-        organization = PartnerOrganization.objects.get(id=organization_id)
-        contact = PartnerContact(
-            last_name=last_name,
-            first_name=first_name,
-            middle_name=middle_name,
-            job_title=job_title,
-            organization=organization,
-            commentary=commentary
-        )
-        contact.save()
+        if 'add_contact' in request.POST:
+            last_name = request.POST["last_name"]
+            first_name = request.POST["first_name"]
+            middle_name = request.POST["middle_name"]
+            job_title = request.POST["job_title"]
+            organization_id = request.POST["organization"]
+            commentary = request.POST["commentary"]
+            organization = PartnerOrganization.objects.get(id=organization_id)
+            contact = PartnerContact(
+                last_name=last_name,
+                first_name=first_name,
+                middle_name=middle_name,
+                job_title=job_title,
+                organization=organization,
+                commentary=commentary
+            )
+            contact.save()
 
-        email = request.POST["email"]
-        email = PartnerContactEmail(email=email, contact=contact)
-        email.save()
+            email = request.POST["email"]
+            email = PartnerContactEmail(email=email, contact=contact)
+            email.save()
 
-        phone = request.POST["phone"]
-        phone = PartnerContactPhone(phone=phone, contact=contact)
-        phone.save()
+            phone = request.POST["phone"]
+            phone = PartnerContactPhone(phone=phone, contact=contact)
+            phone.save()
 
-        projects_id = request.POST.getlist("project")
-        contact.projects.add(*projects_id)
-        contact.save()
+            projects_id = request.POST.getlist("project")
+            contact.projects.add(*projects_id)
+            contact.save()
+        elif 'import_contacts' in request.POST:
+            form = ImportDataForm(request.POST, request.FILES)
+            if form.is_valid():
+                data = imports.contacts(form)
+                message = data[0]
+            else:
+                data = form.errors
+                message = "IndexError"
     contacts = PartnerContact.objects.all()
     projects = Project.objects.all()
     organizations = PartnerOrganization.objects.all()
@@ -45,21 +57,26 @@ def contacts_list(request):
         'contacts': contacts,
         'organizations': organizations,
         'projects': projects,
-        'contacts_count': len(contacts)
+        'contacts_count': len(contacts),
+        'form': ImportDataForm(request.POST, request.FILES),
+        'message': message,
+        'data': data
     })
 
 @csrf_exempt
 def add_organization(request):
     if request.method == 'POST':
         name = request.POST["name"]
+        organization_inn = request.POST["organization_inn"]
         organization_type = request.POST["organization_type"]
         organization = PartnerOrganization(
             name=name,
+            organization_inn=organization_inn,
             organization_type=organization_type
         )
         organization.save()
-    
     return HttpResponseRedirect(reverse("contacts_list"))
+
 
 def send_mailing_list(request):
     pass
