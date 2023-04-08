76628bd01571f1f026eb74a0298264e2362cd43b
BurmAnton <a.burmatsky@gmail.com>
2023-04-08 18:02:19 +0400

    Change MEDIA_URL and MEDIA_ROOT in setting.py, add media files to projects urlpatterns

--- copp163_coordinator/urls.py
+++ copp163_coordinator/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('federal_empl_program.urls')),
@@ -24,3 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('select2/', include('django_select2.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
