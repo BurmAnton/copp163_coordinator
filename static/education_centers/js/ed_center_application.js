fd6757377b2d7539498f12adcf81c769fbb09226
BurmAnton <a.burmatsky@gmail.com>
2023-05-13 14:30:17 +0400

    Divided steps into tabs in application page

--- static/education_centers/js/ed_center_application.js
+++ static/education_centers/js/ed_center_application.js
@@ -1,7 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.collapse-img').forEach(collapse =>{
-        collapse.addEventListener('click', (img) => {
-            img.srcElement.classList.toggle("rot");
+    document.querySelectorAll('.step-btn').forEach(step =>{
+        step.addEventListener('click', (btn) => {
+            document.querySelector('.step-btn.btn-primary').classList.add("btn-outline-primary");
+            document.querySelector('.step-btn.btn-primary').classList.remove("btn-primary");
+            btn.srcElement.classList.add("btn-primary");
+            btn.srcElement.classList.remove("btn-outline-primary");
+
+            let steps = document.querySelectorAll('.step')
+            steps.forEach(step =>{
+                step.style.display = 'none';
+                if (step.dataset.step == btn.srcElement.dataset.step) {
+                    step.style.display = 'block';
+                }
+            })
         })
     })
 })
