5874d0789c317e2af3a006492483261a5c3b8bc9
BurmAnton <a.burmatsky@gmail.com>
2023-05-16 18:46:10 +0400

    Now application page remember current stage after refresh

--- static/education_centers/js/ed_center_application.js
+++ static/education_centers/js/ed_center_application.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
+    window.history.replaceState(null, null, `?s=${document.querySelector('.step-btn.btn-primary').dataset.step}`);
     document.querySelectorAll('.step-btn').forEach(step =>{
         step.addEventListener('click', (btn) => {
             document.querySelector('.step-btn.btn-primary').classList.add("btn-outline-primary");
             document.querySelector('.step-btn.btn-primary').classList.remove("btn-primary");
             btn.srcElement.classList.add("btn-primary");
-            btn.srcElement.classList.remove("btn-outline-primary");
+            btn.srcElement.classList.remove("btn-outline-primary"); 
 
+            window.history.replaceState(null, null, `?s=${btn.srcElement.dataset.step}`);
             let steps = document.querySelectorAll('.step')
+            
             steps.forEach(step =>{
                 step.style.display = 'none';
                 if (step.dataset.step == btn.srcElement.dataset.step) {
