29f9ed0aab306e918a0beafbc865b41bfa0c5792
BurmAnton <a.burmatsky@gmail.com>
2023-06-14 11:53:15 +0400

    Add quotas dashboard to nav-menu, add value/approved_value to ter_admin quota table

--- future_ticket/views.py
+++ future_ticket/views.py
@@ -31,34 +31,44 @@ def quotas(request):
     centers_project_year = EducationCenterTicketProjectYear.objects.filter(
         project_year=project_year
     )
-    full_quota = get_object_or_404(TicketFullQuota, project_year=project_year)
     quota_stat = dict()
     quota_stat_all = dict()
     quota_stat_all['full_qouta'] = 0
     quota_stat_all['federal_quota'] = 0
     quota_stat_all['none_federal_quota'] = 0
+    quota_stat_all['approved_full_qouta'] = 0
+    quota_stat_all['approved_federal_quota'] = 0
+    quota_stat_all['approved_none_federal_quota'] = 0
     for ter_admin in School.TER_CHOICES:
         quota_stat[ter_admin[1]] = dict()
-        ter_admin_schools = School.objects.filter(
-            territorial_administration=ter_admin[0]
-        )
-        schools_quota = TicketQuota.objects.filter(
-            school__in=ter_admin_schools).distinct()
-        quota_stat[ter_admin[1]]['federal_quota'] = schools_quota.filter(
-            is_federal=True).distinct().aggregate(Sum("approved_value"))['approved_value__sum']
-        if quota_stat[ter_admin[1]]['federal_quota'] == None:
-            quota_stat[ter_admin[1]]['federal_quota'] = 0
-        quota_stat[ter_admin[1]]['none_federal_quota'] = schools_quota.filter(
-            is_federal=False).distinct().aggregate(Sum("approved_value"))['approved_value__sum']
-        if quota_stat[ter_admin[1]]['none_federal_quota'] == None:
-            quota_stat[ter_admin[1]]['none_federal_quota'] = 0
-        quota_stat[ter_admin[1]]['full_quota'] = schools_quota.distinct().aggregate(
-            Sum("approved_value"))['approved_value__sum']
-        if quota_stat[ter_admin[1]]['full_quota'] == None:
-            quota_stat[ter_admin[1]]['full_quota'] = 0
-        quota_stat_all['full_qouta'] += quota_stat[ter_admin[1]]['full_quota']
-        quota_stat_all['federal_quota'] += quota_stat[ter_admin[1]]['federal_quota']
-        quota_stat_all['none_federal_quota'] += quota_stat[ter_admin[1]]['none_federal_quota']
+        ter_admin_schools = School.objects.filter(territorial_administration=ter_admin[0])
+        schools_quota = TicketQuota.objects.filter(school__in=ter_admin_schools).distinct()
+        
+        federal_quota = schools_quota.filter(is_federal=True).distinct().aggregate(Sum("approved_value"), Sum("value"))
+        if federal_quota['value__sum'] == None: federal_quota['value__sum'] = 0
+        if federal_quota['approved_value__sum'] == None: federal_quota['approved_value__sum'] = 0
+        quota_stat[ter_admin[1]]['approved_federal_quota'] = federal_quota['approved_value__sum']
+        quota_stat[ter_admin[1]]['federal_quota'] = federal_quota['value__sum']
+        
+        none_federal_quota = schools_quota.filter(is_federal=False).distinct().aggregate(Sum("approved_value"), Sum("value"))
+        if none_federal_quota['value__sum'] == None: none_federal_quota['value__sum'] = 0
+        if none_federal_quota['approved_value__sum'] == None: none_federal_quota['approved_value__sum'] = 0
+        quota_stat[ter_admin[1]]['none_federal_quota'] = none_federal_quota['value__sum']
+        quota_stat[ter_admin[1]]['approved_none_federal_quota'] = none_federal_quota['approved_value__sum']
+        
+        full_quota = schools_quota.distinct().aggregate(Sum("approved_value"), Sum("value"))
+        if full_quota['value__sum'] == None: full_quota['value__sum'] = 0
+        if full_quota['approved_value__sum'] == None: full_quota['approved_value__sum'] = 0
+        quota_stat[ter_admin[1]]['full_quota'] = full_quota['value__sum']
+        quota_stat[ter_admin[1]]['approved_full_quota'] = full_quota['approved_value__sum']
+        
+        quota_stat_all['full_qouta'] += full_quota['value__sum']
+        quota_stat_all['federal_quota'] += federal_quota['value__sum']
+        quota_stat_all['none_federal_quota'] += none_federal_quota['value__sum']
+        quota_stat_all['approved_full_qouta'] += full_quota['value__sum']
+        quota_stat_all['approved_federal_quota'] += federal_quota['approved_value__sum']
+        quota_stat_all['approved_none_federal_quota'] += none_federal_quota['approved_value__sum']
+    full_quota = get_object_or_404(TicketFullQuota, project_year=project_year)
     quotas = TicketQuota.objects.exclude(value=0)
     ed_centers = EducationCenter.objects.exclude(ticket_quotas=None)
     schools = School.objects.exclude(ticket_quotas=None)
