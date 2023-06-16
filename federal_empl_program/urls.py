5474dec2d95d70380512518d6342a95445c25777
BurmAnton <a.burmatsky@gmail.com>
2023-06-16 14:37:09 +0400

    Remove some legasy models and views

--- federal_empl_program/urls.py
+++ federal_empl_program/urls.py
@@ -6,25 +6,9 @@ urlpatterns = [
     path('',views.index, name='index'),
     #Import
     path('import/express/', views.import_express, name='import_express'),
-    path('import/gd/', views.import_gd, name='import_gd'),
-    path('import/statuses/', views.import_st, name='import_st'),
-
     #Auth
     re_path(r'^login/$', views.login, name="login"),
     path('logout/', views.logout, name="logout"),
-    #registration
-    path('registration/', views.registration, name='registration'),
-    path('registration/<int:stage>', views.reg_stage, name='reg_stage'),
-    path('registration/<int:stage>/?c=<ed_cenret_id>/', views.reg_stage, name='reg_stage'),
-    #pass_recovery
-    path('password/recovery/<int:step>/', views.password_recovery, name="password_recovery"),
-    
-    #applicants
-    path('applicant/profile/<int:user_id>/', views.applicant_profile, name="applicant_profile"),
     #Quotes
-    path('quota/dashboard/', views.quota_dashboard, name="quota_dashboard"),
-    #Groups
-    re_path(r'^groups/$', views.group_list, name='group_list'),
-    path('groups/?c=<ed_cenret_id>/', views.group_list, name='group_list'),
-    path('groups/select/', views.group_select, name='group_select')
+    path('quota/dashboard/', views.quota_dashboard, name="quota_dashboard")
 ]
