078e7dbc17ac853947e33673d069d222519a16ba
BurmAnton <a.burmatsky@gmail.com>
2023-08-04 15:42:28 +0400

    Add applications_dashboard view

---
+++ federal_empl_program/templatetags/fed_extras.py
@@ -0,0 +1,34 @@
+from django import template
+from django.shortcuts import get_object_or_404
+from django.template.defaultfilters import stringfilter
+
+
+register = template.Library()
+
+@register.filter
+def count_appl(applications, params):
+    stage, grant = params[0], params[1]
+    if grant == None:
+        applications = applications.filter(stage=stage)
+    else:
+        applications = applications.filter(grant=grant, stage=stage)
+    return len(applications)
+
+@register.filter
+def count_grant_appl(applications, grant_qoute):
+    ratio = f'{applications}/{(grant_qoute.qouta_72 + grant_qoute.qouta_144 + grant_qoute.qouta_256)}'
+    procent = f'{int(applications / (grant_qoute.qouta_72 + grant_qoute.qouta_144 + grant_qoute.qouta_256) * 100)}%'
+    return f'{ratio} ({procent})'
+
+@register.filter
+def count_qouta(applications, duration, grant=None):
+    if grant == None:
+        applications = applications.filter(
+            education_program__duration=duration
+        )
+    else:
+        applications = applications.filter(
+            education_program__duration=duration,
+            grant=grant
+        )
+    return len(applications)
