118e9ff38bbc80057a8807ab2fc0f79d0d0e3584
BurmAnton <a.burmatsky@gmail.com>
2023-08-11 15:34:15 +0400

    Add schools export in school admin view

--- citizens/urls.py
+++ citizens/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('schools/import/', views.import_schools, name='import_schools')
+    path('schools/import/', views.import_schools, name='import_schools'),
+    path('export/schools/', views.export_schools, name='export_schools'),
 ]
