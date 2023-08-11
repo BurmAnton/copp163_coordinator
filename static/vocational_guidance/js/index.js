c08f021da9ec239e9d0b0715df69c8db74fb38a5
BurmAnton <a.burmatsky@gmail.com>
2023-08-11 11:47:09 +0400

    Adapted schools_application page for mobile devices

--- static/vocational_guidance/js/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#disability-check').addEventListener('change', (event) => {
-        if (event.currentTarget.checked) {
-            document.querySelector('#form-disability').style.display = "block";
-        } else {
-            document.querySelector('#form-disability').style.display = "none";
-        }
-    })
-})
