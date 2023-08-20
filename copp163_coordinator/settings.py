2e7ba19319c1cc5cde80e0a97cee8d7d627694b2
BurmAnton <a.burmatsky@gmail.com>
2023-08-20 13:43:54 +0400

    Add STATIC_URL to .env

--- copp163_coordinator/settings.py
+++ copp163_coordinator/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/copp163_coordinator/static/'
+STATIC_URL = os.environ.get("STATIC_URL")
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     BASE_DIR / "copp163_coordinator/static",
