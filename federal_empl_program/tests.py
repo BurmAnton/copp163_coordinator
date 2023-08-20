72e503dc84814f8f056c9ffdcce4fe3ce7ea62ed
BurmAnton <a.burmatsky@gmail.com>
2023-08-20 13:31:54 +0400

    small fixes

--- federal_empl_program/tests.py
+++ federal_empl_program/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+
+from users.models import User
 
 # Create your tests here.
+class IndexTest(TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+    
+    def test_view(self):
+        # Issue a GET request.
+        response = self.client.get("")
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 302)
+
+
+class ImportExpressTest(TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        User.objects.create_user(email='testuser@test.com', password='12345')
+        self.client = Client()
+        
+    def test_view(self):
+        # Issue a GET request.
+        self.client.login(username='testuser@test.com', password='12345') 
+        response = self.client.get("/import/express/")
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
