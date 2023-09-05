d569e549740e97e0b99ccbdb2bfedfeb88f690b6
BurmAnton <a.burmatsky@gmail.com>
2023-09-05 16:25:26 +0400

    add created_at and updated_at fields to TicketProgram

--- copp163_coordinator/settings.py
+++ copp163_coordinator/settings.py
@@ -60,7 +60,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djaa_list_filter',
     'mathfilters',
     'django_cleanup.apps.CleanupConfig'
 ]
