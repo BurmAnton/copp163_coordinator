c06e40b7466de11e4f5c41738c19ef2e53f6042d
Admin <admin@194-58-109-116.cloudvps.regruhosting.ru>
2023-09-27 12:40:46 +0300

    Fix media route in prod

--- copp163_coordinator/settings.py
+++ copp163_coordinator/settings.py
@@ -165,7 +165,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR
-MEDIA_URL = '/'
+MEDIA_URL =  os.environ.get("MEDIA_URL")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
