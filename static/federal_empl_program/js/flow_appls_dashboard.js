5622ead6cc028702cb40a7df5bccabf2539df9be
BurmAnton <a.burmatsky@gmail.com>
2023-10-12 11:32:36 +0400

    Upgrage flow_applications dashboard

---
+++ static/federal_empl_program/js/flow_appls_dashboard.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.show-appls').addEventListener('click', () =>{
+        document.querySelector('.show-appls').style.display = 'none';
+        document.querySelector('.hide-appls').style.display = 'block';
+        document.querySelectorAll('.qouta-duration').forEach(th => {
+            th.setAttribute("colspan", 4)
+        });
+        document.querySelectorAll('.hide-col').forEach(th => {
+            th.classList.add('show-col')
+        });
+    })
+    document.querySelector('.hide-appls').addEventListener('click', () =>{
+        document.querySelector('.hide-appls').style.display = 'none';
+        document.querySelector('.show-appls').style.display = 'block';
+        document.querySelectorAll('.qouta-duration').forEach(th => {
+            th.setAttribute("colspan", 3)
+        });
+        document.querySelectorAll('.hide-col').forEach(th => {
+            th.classList.remove('show-col')
+        });
+    })
+})
