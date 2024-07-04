2e4711a46d8898520ee18787093b6be25fef6af4
BurmAnton <a.burmatsky@gmail.com>
2024-07-04 17:56:38 +0400

    Change Group admin change

--- education_centers/admin.py
+++ education_centers/admin.py
@@ -216,7 +216,7 @@ class GroupAdmin(admin.ModelAdmin):
     
     search_fields = ['name', 'start_date', 'end_date']
     list_display = ('education_program', 'ed_center', 'start_date', 'end_date')
-    fieldsets = ('education_program', 'ed_center', 'start_date', 'end_date')
+    #fieldsets = ('education_program', 'ed_center', 'start_date', 'end_date')
 
     def ed_center(self, group):
         if group.education_center == None:
